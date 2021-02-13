238fcb5aa30337e7509e1323555d0c0e4c9435de
brdnkva <bev030997@gmail.com>
2021-02-13 20:17:47 +0300

    add simulator structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import Simulator from "./components/simulator";
 
 function App() {
-  return (
-    <div className="App">
+    const text = `Some quick example text to build on the card title and make up the bulk of the card's content.`;
 
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark">
+                <Nav className="mr-auto">
+                </Nav>
+            </Navbar>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <h3>Тренажер</h3>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Simulator text={text}/>
+                </Row>
+                <Row className="justify-content-md-center">
+                    info
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
