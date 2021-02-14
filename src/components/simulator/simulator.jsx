42e5c38d3e4b007152f2c3d3ab3efc25ac11d53f
brdnkva <bev030997@gmail.com>
2021-02-14 17:42:50 +0300

    add a replacement for the last invalid character

--- src/components/simulator/simulator.jsx
+++ src/components/simulator/simulator.jsx
@@ -8,14 +8,30 @@ const Simulator = ({text}) => {
     const [inputUser, setInputUser] = React.useState('');
 
     const handleInput = (event) => {
-        setInputUser(event.target.value);
+        const length = inputUser.length - 1;
+        const value = event.target.value;
+
+         if (inputUser.length === 0 ||
+             inputUser[length] === textArr[length]) {
+             setInputUser(value);
+         }
+         else {
+             if (value.length === 2) {
+                  setInputUser(value.substring(1, 2))
+             } else {
+                 setInputUser(value.substring(0, value.length - 2) + value.substring(value.length - 1))
+             }
+         }
     }
 
     return (
         <div>
+            <Card.Body>
+                <Card.Text>Введите текст в область ввода. Время начнется тогда, когда будет введен первый символ, и закончится, когда будет введен последний правильный символ. Удачи!</Card.Text>
             <InputGroup className="mb-3">
                 <FormControl
                     as={'textarea'}
+                    rows={4}
                     value={inputUser}
                     onChange={handleInput}
                     placeholder={'Начните вводить текст здесь'}
@@ -27,6 +43,7 @@ const Simulator = ({text}) => {
             <Card>
                 <SimulatorInfo/>
             </Card>
+            </Card.Body>
         </div>
     );
 }
